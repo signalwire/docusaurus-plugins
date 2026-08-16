1a7a54eda9b00cbfc25b72d3f66d42d5f8a6cac0
Devon-White <devon.white@signalwire.com>
2026-08-16 10:19:46 -0400

    docs(theme): fix malformed nested block comment in theme-llms-txt.d.ts
    The header opened a second /** inside an already-open block comment, so the
    license line and package description were part of a broken comment structure.

--- packages/docusaurus-theme-llms-txt/src/theme-llms-txt.d.ts
+++ packages/docusaurus-theme-llms-txt/src/theme-llms-txt.d.ts
@@ -1,11 +1,8 @@
 /**
- *
- * /**
  * Copyright (c) SignalWire, Inc. and its affiliates.
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
- *
  *
  * @signalwire/docusaurus-theme-llms-txt
  *
